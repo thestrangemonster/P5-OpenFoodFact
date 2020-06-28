1f51ff539db3dd03136783e1942b9819ba0dd39b
thestrangemonster <thomas.gretten@live.fr>
2020-06-28 14:51:30 +0200

    change => correction pep8=> main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-# us import the files API, 
-# database and app with every 
+# us import the files API,
+# database and app with every
 # method specific to the files
 
 import database
@@ -15,7 +15,7 @@ categories = {1: "pizza",
               6: "fruits"
               }
 
-# var for stocked the limit of each categories              
+# var for stocked the limit of each categories
 length = [0]
 # var for stoked the number of products of each categories
 size = []
@@ -29,19 +29,19 @@ def main():
     db = database
     # methode drop_db() => drop of the database
     db.drop_db()
-    # methode create_db() => creation of the database 
+    # methode create_db() => creation of the database
     db.create_db()
-    # var for stoked the number of the limite 
-    # and size of the products per categories 
+    # var for stoked the number of the limite
+    # and size of the products per categories
     nb = 0
     # loop for each categories used for iterating over a sequence "dictionnary"
     for row in categories:
-        # methode for insert to the db, the datas from api 
+        # methode for insert to the db, the datas from api
         data_from_api = Api(categories[row]).get_data_api()
         nb += data_from_api
         size.append(data_from_api)
         length.append(nb)
-    # instance class App   
+    # instance class App
     app = App()
     # loop for run the app
     while run:
@@ -51,7 +51,7 @@ def main():
         # a loop, if choice is not between 1 and 6
         nb_not_allowed = True
         while nb_not_allowed:
-            # if you press another number or letter 
+            # if you press another number or letter
             try:
                 choice_caterogie = int(
                     input("Enter a number between 1 and 6: "))
@@ -59,8 +59,8 @@ def main():
                 nb_not_allowed = False
 
             except KeyError:
-                print("please enter a between 1 and 6, ", end='') 
-                print("you introduced a wrong number")  
+                print("please enter a between 1 and 6, ", end='')
+                print("you introduced a wrong number")
                 pass
 
             except ValueError:
@@ -70,22 +70,27 @@ def main():
             # condiction for enter in the category
             if choice_categories == categories[row]:
                 app.display_choice_of_products(
-                    row, length[row-1], length[row], size[row-1])
-    
+                                                row,
+                                                length[row-1],
+                                                length[row],
+                                                size[row-1]
+                                            )
         # condition for exit or continue
         if app.run_app is False:
 
             # if you want quit or if you want choice anather category
-            choice_for_exit_or_continue = input("[Q/AC] If you want quit type Q, or if you want choice another category type AC ")
-            if choice_for_exit_or_continue == "Q":
+            stc_one = "[Q/AC] If you want quit type Q, "
+            stc_two = "or if you want choice another category type AC "
+            choice_for_exit_or_choice = input("{}{}".format(stc_one, stc_two))
+            if choice_for_exit_or_choice == "Q":
                 run = False
 
-            if choice_for_exit_or_continue == "AC":
+            if choice_for_exit_or_choice == "AC":
                 app.re_init()
 
-# when python execut this script 
+# when python execut this script
 # he's using one special variable __name__
-# so, if python execut this script... 
+# so, if python execut this script...
 # my function main() can be executed
 if __name__ == "__main__":
 
